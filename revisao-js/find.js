75403f55303dd4eb6e73e0c6f5cb8f6dd401f253
douglasjardini <douglasjardini@gmail.com>
2021-09-01 01:15:21 +0000

    (31/08) Propriedades em REACT e 'key' em listas

---
+++ revisao-js/find.js
@@ -0,0 +1,9 @@
+const nums = [ 10, 19, 3, -4, 13, -11, 15, 0, -1]
+
+// Encontrar o primeiro número negativo presente no vetor
+const primeiroNeg = nums.find(x => x < 0)
+console.log({primeiroNeg})
+
+/*
+    find() retorna o primeiro elemento que se está buscando na function
+*/
---
+++ revisao-js/includes.js
@@ -0,0 +1,9 @@
+const carros = ['Corcel', 'Chevete', 'Opala', 'Fusca', 'Brasilia', 'Spazio']
+
+console.log('Tem Opala?', carros.includes('Opala'))
+console.log('Tem Belina?', carros.includes('Belina'))
+
+/*
+    includes() retorna true caso o argumento passado exista no vetor
+    ou valse, caso contrário.
+*/
